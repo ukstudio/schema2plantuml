6355704c063c94aec6a18bab9d8b101eb58abb8a
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2017-05-23 19:14:36 +0900

    First implementation

--- lib/schema2plantuml.rb
+++ lib/schema2plantuml.rb
@@ -1,5 +1,12 @@
 require "schema2plantuml/version"
+require "Schema2plantuml/active_record/schema"
 
-module Schema2plantuml
-  # Your code goes here...
+class Schema2plantuml
+  def initialize(schema_file_path = 'schema.rb')
+    @schema_file_path = schema_file_path
+  end
+
+  def run
+    load @schema_file_path
+  end
 end
---
+++ lib/schema2plantuml/active_record/schema.rb
@@ -0,0 +1,33 @@
+module ActiveRecord
+  class Schema
+    def self.define(_info, &block)
+      new.define(&block)
+    end
+
+    def define(&block)
+      instance_eval(&block)
+    end
+
+    def create_table(table_name, _options, &block)
+      puts <<~TABLE
+      object #{table_name} {
+        #{block.call(Table2plantuml.new)}
+      }
+      TABLE
+    end
+
+    def add_foreign_key(child_table_name, parent_table_name, _options = {})
+      puts "#{child_table_name} o-- #{parent_table_name}"
+    end
+
+    def enable_extension(*); end
+    def add_index(*); end
+    def create_view(*); end
+  end
+
+  class Table2plantuml
+    def method_missing(column_type, column_name, _options = {})
+      "#{column_name} : #{column_type}"
+    end
+  end
+end
